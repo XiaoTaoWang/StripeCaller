1a0037a7d944960008885532302673b0be2b3a25
Xiaotao Wang <wangxiaotao686@gmail.com>
2019-03-24 22:27:11 -0400

    post processing

--- stripecaller/callers.py
+++ stripecaller/callers.py
@@ -2,6 +2,8 @@ import logging
 import numpy as np
 from scipy import sparse
 from scipy.stats import poisson
+from scipy.signal import convolve2d
+from sklearn.decomposition import PCA
 
 logger = logging.getLogger(__name__)
 
@@ -110,3 +112,165 @@ def _vertical_stripe(M, cM, maxapart, res, l_n, b_n, siglevel, fold, chromLen):
     yi = yi[candi_mask]
 
     return xi, yi
+
+def consecutive_runs(arr):
+
+    arr.sort()
+    pieces = np.split(arr, np.where(np.diff(arr)!=1)[0]+1)
+    maxlen = max(map(len, pieces))
+
+    return pieces, maxlen
+
+def extend_stretches(pieces, min_seed_len=6, max_gap=5, min_stripe_len=9):
+
+    filtered = [p for p in pieces if len(p)>=min_seed_len] # can't be empty
+    stripes = []
+    seed = filtered[0]
+    for p in filtered[1:]:
+        if p[0] - seed[-1] < (max_gap + 2):
+            seed = np.r_[seed, p]
+        else:
+            if seed[-1] - seed[0] + 1 >= min_stripe_len:
+                stripes.append([seed[0], seed[-1]+1])
+            seed = p
+    
+    if seed[-1] - seed[0] + 1 >= min_stripe_len:
+        stripes.append([seed[0], seed[-1]+1])
+    
+    return stripes
+
+def call_stripes(M, cM, maxapart, res, l_n, b_n, siglevel, fold, chromLen,
+                 min_seed_len=6, max_gap=5, min_stripe_len=9):
+    
+    # call horizontal stripes
+    xi, yi = _horizontal_stripe(M, cM, maxapart, res, l_n, b_n, siglevel, fold, chromLen)
+    h_stripes = {}
+    for anchor_x in np.unique(xi):
+        ys = yi[xi==anchor_x]
+        pieces, maxlen = consecutive_runs(ys)
+        if maxlen < min_seed_len:
+            continue
+        tmp = extend_stretches(pieces,
+                               min_seed_len=min_seed_len,
+                               max_gap=max_gap,
+                               min_stripe_len=min_stripe_len)
+        h_stripes[anchor_x] = tmp
+    
+    # call vertical stripes
+    xi, yi = _vertical_stripe(M, cM, maxapart, res, l_n, b_n, siglevel, fold, chromLen)
+    v_stripes = {}
+    for anchor_y in np.unique(yi):
+        xs = xi[yi==anchor_y]
+        pieces, maxlen = consecutive_runs(xs)
+        if maxlen < min_seed_len:
+            continue
+        tmp = extend_stretches(pieces,
+                               min_seed_len=min_seed_len,
+                               max_gap=max_gap,
+                               min_stripe_len=min_stripe_len)
+        v_stripes[anchor_y] = tmp
+
+    return h_stripes, v_stripes
+        
+def remove_compartment_stripes(Lib, chrom, h_stripes, v_stripes, window=10000000, step=5000000, smooth_factor=1):
+
+    res = Lib.binsize
+    min_matrix = 10
+    kernel = np.ones((smooth_factor, smooth_factor))
+
+    # consistent expected value
+    cM = Lib.matrix(balance=True, sparse=True).fetch(chrom)
+    tmp = np.isfinite(Lib.bins().fetch(chrom)['weight'].values)
+    maxdis = min(cM.shape[0]-1, window//res)
+    pre_cal = {}
+    for i in range(maxdis+1):
+        if i > 0:
+            valid = tmp[:-i] * tmp[i:]
+        else:
+            valid = tmp
+        current = cM.diagonal(i)[valid]
+        if current.size > 0:
+            v = current.mean()
+            pre_cal[i] = v
+
+    start = 0
+    while Lib.chromsizes[chrom] - start > res*min_matrix:
+        end = min(Lib.chromsizes[chrom], start+window)
+        mask = np.isnan(Lib.bins().fetch((chrom,start,end))['weight'].values) # gaps
+        tmp = np.logical_not(mask) # valid rows or columns
+        if tmp.sum() < min_matrix:
+            continue
+        cM = Lib.matrix(balance=True, sparse=False).fetch((chrom, start, end))
+        cM[np.isnan(cM)] = 0
+        # smooth matrix
+        smooth = convolve2d(cM, kernel, mode='same')
+        # expected matrix
+        expected = np.zeros_like(cM)
+        idx = range(expected.shape[0])
+        for i in idx:
+            if i in pre_cal:
+                if i > 0:
+                    expected[idx[:-i], idx[i:]] = pre_cal[i]
+                    expected[idx[i:], idx[:-i]] = pre_cal[i]
+                else:
+                    expected[idx, idx] = pre_cal[i]
+        expected = convolve2d(expected, kernel, mode='same') # smooth the expected matrix with same smoothing factor
+        # PCA
+        obs_exp = np.zeros_like(expected)
+        obs_exp[expected!=0] = smooth[expected!=0] / expected[expected!=0]
+        # remove gaps
+        index = list(np.where(mask)[0])
+        convert_index = np.where(np.logical_not(mask))[0]
+        temp = np.delete(obs_exp, index, 0)
+        new_obs_exp = np.delete(temp, index, 1)
+        # pearson correlation matrix
+        pearson = np.corrcoef(new_obs_exp)
+        pca = PCA(n_components=3, whiten=True, svd_solver='arpack')
+        fp = pca.fit_transform(pearson)[:,0]
+        # map back to original coordinates
+        n = obs_exp.shape[0]
+        arr = np.zeros(n)
+        arr[convert_index] = fp # PC1
+
+        code = arr > 0
+        # correct horizontal stripes
+        for h in h_stripes:
+            rh = h - start//res
+            if (rh < 0) or (rh + 1 > arr.size):
+                continue
+            tmp = h_stripes[h]
+            count = 0
+            for i in range(tmp[0], tmp[1]):
+                ri = i - start//res
+                if (ri < 0) or (ri + 1 > arr.size):
+                    continue
+                if (code[rh]==code[ri]) and (code[rh:ri+1].sum() > 0) and (code[rh:ri+1].sum() < code[rh:ri+1].size):
+                    count += 1
+            if count / (tmp[1]-tmp[0]) > 0.3:
+                del h_stripes[h]
+        
+        # correct vertical stripes
+        for v in v_stripes:
+            rv = v - start//res
+            if (rv < 0) or (rv + 1 > arr.size):
+                continue
+            tmp = v_stripes[v]
+            count = 0
+            for i in range(tmp[0], tmp[1]):
+                ri = i - start//res
+                if (ri < 0) or (ri + 1 > arr.size):
+                    continue
+                if (code[rv]==code[ri]) and (code[ri:rv+1].sum() > 0) and (code[ri:rv+1].sum() < code[ri:rv+1].size):
+                    count += 1
+            if count / (tmp[1]-tmp[0]) > 0.3:
+                del v_stripes[v]
+    
+    return h_stripes, v_stripes
+
+
+
+
+
+
+
+
